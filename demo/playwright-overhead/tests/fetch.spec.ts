bc7dcb8f9ad06e18ace537f62348561b9aa3fd63
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-01-05 17:47:56 +0100

    fix: tag in playwright test

--- demo/playwright-overhead/tests/fetch.spec.ts
+++ demo/playwright-overhead/tests/fetch.spec.ts
@@ -11,7 +11,7 @@ import { test, expect } from "@playwright/test";
 });
 
 [0, 100, 200, 400, 800, 1600, 3200, 6400, 12800].forEach((delay) => {
-  test(`[MOCKED] can fetch with ${delay}ms delay`, async ({ page }) => {
+  test(`can fetch with ${delay}ms delay @mocked`, async ({ page }) => {
     await page.route(`*/**/api/${delay}`, async (route) => {
       await route.fulfill({ body: "Blazingly mocked!" });
     });
