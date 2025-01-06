3b3492b89857e03067618546dc32d418f942ce30
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-01-06 14:25:22 +0100

    move: separate mocked tests and not mocked tests

--- demo/playwright-overhead/tests/mocked.spec.ts
+++ demo/playwright-overhead/tests/mocked.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 [0, 100, 200, 400, 800, 1600, 3200, 6400, 12800].forEach((delay) => {
-  test(`can fetch with ${delay}ms delay`, async ({ page }) => {
+  test(`can fetch with ${delay}ms delay @mocked`, async ({ page }) => {
+    await page.route(`*/**/api/${delay}`, async (route) => {
+      await route.fulfill({ body: "Blazingly mocked!" });
+    });
     await page.goto(`http://localhost:3000/${delay}`);
     const response = page.waitForResponse(/api/);
     await page.getByRole("button").click();
