874add5e0e0248dba69327131fde1b848291c7a9
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-01-19 22:01:23 +0100

    test demo

--- demo/playwright-tests/tests/simple.spec.ts
+++ demo/playwright-tests/tests/simple.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("should display a good title", async ({ page }) => {
+test("should it contain the right title", async ({ page }) => {
   await page.goto("http://localhost:3000");
-  await expect(page.getByRole("heading")).toHaveText("API Fetch Demo");
+  expect(await page.content()).toMatch("Hakuna Matata");
 });
