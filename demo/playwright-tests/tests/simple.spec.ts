5b735e1d01c7f37e5d57c387985f5c8821d2eabd
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2025-01-23 10:47:05 +0100

    test title demo

--- demo/playwright-tests/tests/simple.spec.ts
+++ demo/playwright-tests/tests/simple.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("should it contain the right title", async ({ page }) => {
+test("should contain the right title", async ({ page }) => {
   await page.goto("http://localhost:3000");
   expect(await page.content()).toMatch("Hakuna Matata");
 });
